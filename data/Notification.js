1e0cf6ca2f6ac33a2675ef3c98f44f37cd940af0
KylixMedusa <ayush.agarwalmng@gmail.com>
2023-07-14 07:06:19 +0530

    first commit

---
+++ data/Notification.js
@@ -0,0 +1,28 @@
+const Notification = [
+  {
+    id: 1,
+    sender: "Rishi Chopra",
+    message: `Mauris blandit erat id nunc blandit, ac eleifend dolor pretium.`,
+    time: "Today at 8:47 PM",
+  },
+  {
+    id: 2,
+    sender: "Neha Kannned",
+    message: `Proin at elit vel est condimentum elementum id in ante. Maecenas et sapien metus.`,
+    time: "Yesterday at 8:30 PM",
+  },
+  {
+    id: 3,
+    sender: "Nirmala Chauhan",
+    message: `Morbi maximus urna lobortis elit sollicitudin sollicitudieget elit vel pretium.`,
+    time: "Yesterday at 4:10 PM",
+  },
+  {
+    id: 4,
+    sender: "Sina Ray",
+    message: `Sed aliquam augue sit amet mauris volutpat hendrerit sed nunc eu diam.`,
+    time: "Yesterday at 12:30 AM",
+  },
+];
+
+export default Notification;
